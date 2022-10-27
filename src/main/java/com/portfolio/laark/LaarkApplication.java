16d85b35925a5b784a696e80815c144c1e069a9e
JungLaark <chenone839@gmail.com>
2022-10-28 08:19:41 +0900

    first commit

---
+++ src/main/java/com/portfolio/laark/LaarkApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.laark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/laark/LaarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.laark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
